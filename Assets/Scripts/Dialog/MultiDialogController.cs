264c99269bfa8428a19fc73720f2af043ee30d27
sebastianMF <sebastian.molano@eu.apptical.com>
2023-03-11 23:51:56 -0500

    Fixed dialog event end invocation

--- Assets/Scripts/Dialog/MultiDialogController.cs
+++ Assets/Scripts/Dialog/MultiDialogController.cs
@@ -11,7 +11,7 @@ public class MultiDialogController : MonoBehaviour
     [SerializeField]
     public DialogProgressionSO dialogProgressionSO;
 
-    UnityEvent onMultiDialogEnd;
+    public UnityEvent onMultiDialogEnd;
 
     DialogController dialogController;
 
