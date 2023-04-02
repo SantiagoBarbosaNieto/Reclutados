161e4ff6a305f05e639f4f1a884708d614f7b7af
Alexander3527 <srodri0715@gmail.com>
2023-04-02 13:51:23 -0500

    Se agregan eventos aleatorios adicionales para la escena del camino

--- Assets/Scripts/Level/ScreenMessage.cs
+++ Assets/Scripts/Level/ScreenMessage.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using ScriptableObjectArchitecture;
+using System;
 
 public class ScreenMessage : MonoBehaviour {
     public GameObject popUpBox;
@@ -11,20 +12,38 @@ public class ScreenMessage : MonoBehaviour {
 
     public void PopUp() {
 
-        int dinero = Random.Range(1, 5);
-        Debug.Log("EVENT CALLED");
-        TransitionItem item = new TransitionItem(PrefsManager.Instance.GetDay(), 1, "Dinero que encontraste", dinero);
+        int dinero = UnityEngine.Random.Range(1, 5);
+        int opcion = UnityEngine.Random.Range(0, 3);
 
-        gameEvent.Raise(item);
-
-        PrefsManager.Instance.AddMoney(dinero);
+        String[] posibleEventsDescription = new String[4];
+        posibleEventsDescription[0] = "¿...? Escuchas sonidos extraños viniendo del bosque. Será mejor ignorarlos...";
+        posibleEventsDescription[1] = "*Te limpias el sudor de la frente* ¡El calor es insoportable!";
+        posibleEventsDescription[2] = "*Ves algo en el camino* ¡Encontraste $" + dinero + " en el suelo!";
+        posibleEventsDescription[3] = "*Tropiezas con una roca* ¡Se te cayeron $" + dinero + " del bolsillo!";
 
         if(PrefsManager.Instance.getEventHappended() == 0) {
+
+            Debug.Log("OPCIÓN ");
+            Debug.Log(opcion);
+
             player.SetActive(false);
             popUpBox.SetActive(true);
-            popUpText.text = "¡Encontraste $" + dinero + " en el suelo!";
-        }
-        PrefsManager.Instance.eventHappendedTrue();       
+            popUpText.text = posibleEventsDescription[opcion];
+            PrefsManager.Instance.eventHappendedTrue();
+
+            Debug.Log("EVENT CALLED");
+
+            if(opcion == 2) {
+                TransitionItem item = new TransitionItem(PrefsManager.Instance.GetDay(), 1, "Dinero que encontraste", dinero);
+                gameEvent.Raise(item);
+                PrefsManager.Instance.UpdateMoney(dinero);
+            }
+            else if(opcion == 3) {
+                TransitionItem item = new TransitionItem(PrefsManager.Instance.GetDay(), 1, "Dinero que perdiste", -dinero);
+                gameEvent.Raise(item);
+                PrefsManager.Instance.UpdateMoney(-dinero);
+            }
+        }    
     }
 
     public void ClosePopUp() {
