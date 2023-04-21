6eea07acdb70671386cb232bf9be8b1d90186402
Alexander3527 <srodri0715@gmail.com>
2023-04-21 11:02:11 -0500

    Opciones de menú y prootipo de sistema de carga y guardado con notificaciones

--- Assets/Scripts/DataPersistence/GameLoader.cs
+++ Assets/Scripts/DataPersistence/GameLoader.cs
@@ -1,9 +1,10 @@
+using TMPro;
+using System.IO;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
+using ScriptableObjectArchitecture;
 using System.Runtime.Serialization.Formatters.Binary;
-using TMPro;
-using UnityEngine;
 
 public class GameLoader : MonoBehaviour {
     public string saveDirectory = "Saves";
@@ -11,10 +12,26 @@ public class GameLoader : MonoBehaviour {
     public GameObject notificationObject;
     public TextMeshProUGUI notificationTitle;
     public TextMeshProUGUI notificationContent;
+
+    [SerializeField]
+    private DaySO day1;
+    [SerializeField]
+    private DaySO day2;
+    [SerializeField]
+    private DaySO day3;
+    [SerializeField]
+    private DaySO day4;
+    [SerializeField]
+    private DaySO day5;
+
+    
+    [SerializeField]
+    private LoadDayRequestGameEvent loadDay;
     
     public void LoadFromFile() {
 
         string path = saveDirectory + "/" + saveName + ".bin";
+        LoadDayRequest request;
 
         Debug.Log("LOADING DATA...");
 
@@ -42,7 +59,52 @@ public class GameLoader : MonoBehaviour {
                 PrefsManager.Instance.UpdateEndBranch(loadData.endBranch);
                 PrefsManager.Instance.UpdateCollaboration(loadData.collaboration);
 
-                
+                switch (loadData.day) {
+                    case 1:
+                        Debug.Log("Attempting to load day 1... ");
+                        request = new LoadDayRequest(day1);
+                        PrefsManager.Instance.ResetPrefs();
+                        loadDay.Raise(request);
+                        Debug.Log("Day 1 loaded!");
+                        break;
+                    
+                    case 2:
+                    Debug.Log("Attempting to load day 2... ");
+                        request = new LoadDayRequest(day2);
+                        PrefsManager.Instance.ResetPrefs();
+                        loadDay.Raise(request);
+                        Debug.Log("Day 2 loaded!");
+                        break;
+                    
+                    case 3:
+                        Debug.Log("Attempting to load day 3... ");
+                        request = new LoadDayRequest(day3);
+                        PrefsManager.Instance.ResetPrefs();
+                        loadDay.Raise(request);
+                        Debug.Log("Day 3 loaded!");
+                        break;
+
+                    case 4:
+                        Debug.Log("Attempting to load day 4... ");
+                        request = new LoadDayRequest(day4);
+                        PrefsManager.Instance.ResetPrefs();
+                        loadDay.Raise(request);
+                        Debug.Log("Day 4 loaded!");
+                        break;
+
+                    case 5:
+                        Debug.Log("Attempting to load day 5... ");
+                        request = new LoadDayRequest(day5);
+                        PrefsManager.Instance.ResetPrefs();
+                        loadDay.Raise(request);
+                        Debug.Log("Day 5 loaded!");
+                        break;
+
+                    default:
+                        PrefsManager.Instance.ResetPrefs();
+                        Debug.Log("Loading error: Day not found");
+                        break;
+                }
             }
             catch {
                 notificationTitle.text = "Error al cargar partida";
