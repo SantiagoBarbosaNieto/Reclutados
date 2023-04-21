a2616f43aba4700b32e2d80c60bf8fa22fc0c991
Santiago <48741013+Lysmmork@users.noreply.github.com>
2023-04-21 15:35:00 -0500

    Se agregan efectos de sonidos a la granja y camino

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -5,15 +5,20 @@ public class PlayerAnimation : MonoBehaviour {
 
     [Header("Dependencies")]
     public Animator animator;
+    public AudioSource steps;
 
     public void OnMovement(InputAction.CallbackContext value) {
         float movementInput = value.ReadValue<Vector2>().magnitude;
 
         if (movementInput > 0f) {
             animator.SetBool("IsRunning", true);
+            if(!steps.isPlaying) {
+                steps.Play();
+            }
         }
         else {
             animator.SetBool("IsRunning", false);
+            steps.Stop();
         }
 
         // animator.SetBool("IsRunning", movementInput != Vector2.zero);
