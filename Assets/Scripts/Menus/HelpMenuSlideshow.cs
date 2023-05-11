71ebf5c5e6929a4fdbb42ab22619f8ec574c594b
Santiago <48741013+Lysmmork@users.noreply.github.com>
2023-05-11 18:59:18 -0500

    Se habilita la opción de seleccionar el slide de inicio y final en el menú de ayuda

--- Assets/Scripts/Menus/HelpMenuSlideshow.cs
+++ Assets/Scripts/Menus/HelpMenuSlideshow.cs
@@ -7,31 +7,44 @@ using UnityEditor;
 public class HelpMenuSlideshow : MonoBehaviour {
 
     public Sprite[] images; // Arreglo de imágenes
+    public int minIndex;
+    public int maxIndex;
     private int currentImageIndex = 0; // Índice de la imagen actual
 
     void Start() {
+
+        if(maxIndex > images.Length - 1) {
+            maxIndex = images.Length - 1;
+        }
+
+        if(minIndex < 0) {
+            minIndex = 0;
+        }
+        
+        currentImageIndex = minIndex;
+
         GetComponent<Image>().sprite = images[currentImageIndex]; // Mostrar la primera imagen en pantalla al inicio
         Debug.Log("START");
     }
 
     public void Next() {
         currentImageIndex += 1;
-        if(currentImageIndex > images.Length-1) {
-            currentImageIndex = 0;
+        if(currentImageIndex > maxIndex) {
+            currentImageIndex = minIndex;
         }
         GetComponent<Image>().sprite = images[currentImageIndex];
     }
 
     public void Previous() {
         currentImageIndex -= 1;
-        if(currentImageIndex < 0) {
-            currentImageIndex = images.Length-1;
+        if(currentImageIndex < minIndex) {
+            currentImageIndex = maxIndex;
         }
         GetComponent<Image>().sprite = images[currentImageIndex];
     }
 
     public void ResetSlides() {
-        currentImageIndex = 0;
+        currentImageIndex = minIndex;
         GetComponent<Image>().sprite = images[currentImageIndex];
     }
 
